7a537d06d098e4546b9830d0bc5c934935aac60d
André Oliveira <andrelpo573@gmail.com>
2022-06-27 20:58:12 -0300

    feat: illustration for empty route and loading animation from video loading

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -10,6 +10,21 @@ import { Button } from "./Button";
 import { CardButton } from "./CardButton";
 import "@vime/core/themes/default.css";
 import { useGetLessonBySlugQuery } from "../graphql/generated";
+import {
+  Audio,
+  BallTriangle,
+  Bars,
+  Circles,
+  Grid,
+  Hearts,
+  Oval,
+  Puff,
+  Rings,
+  SpinningCircles,
+  TailSpin,
+  ThreeDots,
+} from "@agney/react-loading";
+import { CoffeeIcon } from "./CoffeeIcon";
 
 interface VideoProps {
   lessonSlug: string;
@@ -22,76 +37,82 @@ export function Video(props: VideoProps) {
     },
   });
 
-  console.log(data);
-
-  return loading ? (
-    <strong className="flex-1">Carregando...</strong>
-  ) : (
+  return (
     <div className="flex-1">
-      <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
-          <Player>
-            <Youtube
-              videoId={data?.lesson!.videoId!}
-              key={data?.lesson!.videoId!}
-            />
-            <DefaultUi />
-          </Player>
+      {loading ? (
+        <div className="flex justify-center items-center min-h-screen">
+          <div className="w-16">
+            <Audio />
+          </div>
         </div>
-      </div>
-      <div className="p-8 max-w-[1100px] mx-auto">
-        <section className="flex items-start gap-16">
-          <div className="flex-1">
-            <h1 className="text-2xl font-bold">{data?.lesson!.title}</h1>
-            <p className="mt-4 text-gray-200 leading-relaxed">
-              {data?.lesson!.description}
-            </p>
-
-            {data?.lesson?.teacher && (
-              <div className="flex items-center gap-4 mt-6">
-                <img
-                  className="h-16 w-16 rounded-full border-2 border-blue-500"
-                  src={`https://github.com/${data.lesson.teacher.avatarURL}.png`}
-                  alt="Foto Avatar"
+      ) : (
+        <>
+          <div className="bg-black flex justify-center">
+            <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+              <Player>
+                <Youtube
+                  videoId={data?.lesson!.videoId!}
+                  key={data?.lesson!.videoId!}
                 />
-                <div className="leading-relaxed">
-                  <strong className="font-bold text-2xl block">
-                    {data.lesson.teacher.name}
-                  </strong>
-                  <span className="text-gray-200 text-sm block">
-                    {data.lesson.teacher.bio}
-                  </span>
-                </div>
+                <DefaultUi />
+              </Player>
+            </div>
+          </div>
+          <div className="p-8 max-w-[1100px] mx-auto">
+            <section className="flex items-start gap-16">
+              <div className="flex-1">
+                <h1 className="text-2xl font-bold">{data?.lesson!.title}</h1>
+                <p className="mt-4 text-gray-200 leading-relaxed">
+                  {data?.lesson!.description}
+                </p>
+
+                {data?.lesson?.teacher && (
+                  <div className="flex items-center gap-4 mt-6">
+                    <img
+                      className="h-16 w-16 rounded-full border-2 border-blue-500"
+                      src={`https://github.com/${data.lesson.teacher.avatarURL}.png`}
+                      alt="Foto Avatar"
+                    />
+                    <div className="leading-relaxed">
+                      <strong className="font-bold text-2xl block">
+                        {data.lesson.teacher.name}
+                      </strong>
+                      <span className="text-gray-200 text-sm block">
+                        {data.lesson.teacher.bio}
+                      </span>
+                    </div>
+                  </div>
+                )}
               </div>
-            )}
-          </div>
-          <div className="flex flex-col gap-4">
-            <Button
-              icon={<DiscordLogo size={24} />}
-              label="Comunidade do Discord"
-              style="primary"
-            />
-            <Button
-              icon={<Lightning size={24} />}
-              label="Acesse o desafio"
-              style="secondary"
-            />
-          </div>
-        </section>
-        <section className="gap-8 mt-20 grid grid-cols-2">
-          <CardButton
-            icon={<FileArrowDown size={40} />}
-            title="Material complementar"
-            content="Acesse o material complementar para acelerar o seu
+              <div className="flex flex-col gap-4">
+                <Button
+                  icon={<DiscordLogo size={24} />}
+                  label="Comunidade do Discord"
+                  style="primary"
+                />
+                <Button
+                  icon={<Lightning size={24} />}
+                  label="Acesse o desafio"
+                  style="secondary"
+                />
+              </div>
+            </section>
+            <section className="gap-8 mt-20 grid grid-cols-2">
+              <CardButton
+                icon={<FileArrowDown size={40} />}
+                title="Material complementar"
+                content="Acesse o material complementar para acelerar o seu
             desenvolvimento"
-          />
-          <CardButton
-            icon={<Image size={40} />}
-            title="Wallpapers Exclusivos"
-            content="Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina"
-          />
-        </section>
-      </div>
+              />
+              <CardButton
+                icon={<Image size={40} />}
+                title="Wallpapers Exclusivos"
+                content="Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina"
+              />
+            </section>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/pages/Evento.tsx
+++ src/pages/Evento.tsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import { CoffeeIcon } from "../components/CoffeeIcon";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/Sidebar";
@@ -11,7 +12,16 @@ export function Evento() {
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex flex-1">
-        {slug ? <Video lessonSlug={slug} /> : <div className="flex-1"></div>}
+        {slug ? (
+          <Video lessonSlug={slug} />
+        ) : (
+          <div className="flex flex-1 items-center justify-center ">
+            <CoffeeIcon size={"500"} />
+            <p className="text-2xl w-80">
+              Prepare seu melhor café e escolha uma das nossas aulas a direita!
+            </p>
+          </div>
+        )}
         <Sidebar />
       </main>
       <Footer />
