4df6c40f8372ecad0586176e5138938b2912dc33
André Oliveira <andrelpo573@gmail.com>
2022-06-27 21:13:27 -0300

    fix: fixed image bug on subscribe page

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -74,7 +74,7 @@ export function Subscribe() {
         </div>
       </div>
       <img
-        src={"/src/assets/code-mockup.png"}
+        src={require("/src/assets/code-mockup.png")}
         className="mt-10"
         alt="Imagens de código"
       />
